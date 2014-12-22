0d0ee3feea114307768e2ebc1922e20cccaecb19
weiyi-bitw <ninpy.weiyi@gmail.com>
2014-12-22 17:20:01 -0500

    add elementizeCGFormat

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='wei-yi.cheng@mssm.edu',
     packages=['varnorm', 'varnorm.test'],
     include_package_data = True,
-    scripts=['bin/normVCF', 'bin/reinstallPkg'],
+    scripts=['bin/normVCF', 'bin/reinstallPkg', 'bin/elementizeCGFormat'],
     url='http://hidysabc.com/blog',
     license='LICENSE.txt',
     description='Variant normalization tool kit',
